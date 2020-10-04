9a12fdfe97d102fc2c0ffa342568e51476a1c47e
Thomas <icecu8e44@gmail.com>
2020-10-04 17:17:35 +0200

    Add theme colors

---
+++ lib/shared/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+MaterialColor color1 = MaterialColor(0xFF825191, color);
+
+Map<int, Color> color = {
+  50: Color.fromRGBO(130, 81, 145, .1),
+  100: Color.fromRGBO(130, 81, 145, .2),
+  200: Color.fromRGBO(130, 81, 145, .3),
+  300: Color.fromRGBO(130, 81, 145, .4),
+  400: Color.fromRGBO(130, 81, 145, .5),
+  500: Color.fromRGBO(130, 81, 145, .6),
+  600: Color.fromRGBO(130, 81, 145, .7),
+  700: Color.fromRGBO(130, 81, 145, .8),
+  800: Color.fromRGBO(130, 81, 145, .9),
+  900: Color.fromRGBO(130, 81, 145, 1),
+};
