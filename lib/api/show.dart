f25de72f3fe22e600953f384e5536720bde3ef06
Thomas <icecu8e44@gmail.com>
2021-02-15 16:19:48 +0100

    Correct mediaStream null if played from list

--- lib/api/show.dart
+++ lib/api/show.dart
@@ -8,7 +8,7 @@ Future<Category> getShowSeasonEpisode(String parentId, String seasonId) async {
   queryParams['seasonId'] = seasonId;
   queryParams['userId'] = user.id;
   queryParams['Fields'] =
-      'ItemCounts,PrimaryImageAspectRatio,BasicSyncInfo,CanDelete,MediaSourceCount,Overview,DateCreated';
+      'ItemCounts,PrimaryImageAspectRatio,BasicSyncInfo,CanDelete,MediaSourceCount,Overview,DateCreated,MediaStreams';
 
   var url = '${server.url}/Shows/${parentId}/Episodes';
 
