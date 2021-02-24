f9726551952fce855c6b38cce15f4e362434f8bf
Thomas <icecu8e44@gmail.com>
2021-02-24 09:10:56 +0100

    Correct unused import...

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:jellyflut/database/database.dart';
 import 'package:jellyflut/provider/musicPlayer.dart';
 import 'package:jellyflut/screens/home/home.dart';
