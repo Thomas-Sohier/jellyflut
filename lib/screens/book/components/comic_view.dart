a3ccb84a0843d39fea43826ea448774d5e16243b
icecu8e <icecu8e44@gmail.com>
2021-12-30 14:43:16 +0100

    on PC scroll to zoom on comic

--- lib/screens/book/components/comic_view.dart
+++ lib/screens/book/components/comic_view.dart
@@ -1,4 +1,5 @@
 import 'package:archive/archive.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 
 import 'package:photo_view/photo_view.dart';
@@ -7,9 +8,11 @@ import 'package:responsive_builder/responsive_builder.dart';
 
 class ComicView extends StatelessWidget {
   final PageController controller;
+  final PhotoViewController _photoViewController = PhotoViewController();
   final Archive archive;
   final Function(int currentPage, int nbPage) listener;
-  const ComicView(
+
+  ComicView(
       {Key? key,
       required this.controller,
       required this.archive,
@@ -20,22 +23,35 @@ class ComicView extends StatelessWidget {
   Widget build(BuildContext context) {
     final nbPages = archive.files.length;
 
-    return PhotoViewGallery.builder(
-        itemCount: nbPages,
-        enableRotation: false,
-        gaplessPlayback: true,
-        scrollDirection: Axis.horizontal,
-        onPageChanged: (index) => listener(index, nbPages),
-        pageController: controller,
-        builder: (context, index) {
-          final content =
-              archive.files.elementAt(index).rawContent as InputStream;
-          return PhotoViewGalleryPageOptions(
-            imageProvider: MemoryImage(content.toUint8List()),
-            filterQuality: FilterQuality.high,
-            initialScale: PhotoViewComputedScale.contained,
-          );
-        });
+    return Listener(
+        onPointerSignal: (pointerSignal) {
+          if (pointerSignal is PointerScrollEvent) {
+            if (pointerSignal.scrollDelta.dy.isNegative) {
+              final s = (_photoViewController.scale ?? 1) + 0.1;
+              _photoViewController.updateMultiple(scale: s);
+            } else {
+              final s = (_photoViewController.scale ?? 1) - 0.1;
+              _photoViewController.updateMultiple(scale: s);
+            }
+          }
+        },
+        child: PhotoViewGallery.builder(
+            itemCount: nbPages,
+            enableRotation: false,
+            gaplessPlayback: true,
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (index) => listener(index, nbPages),
+            pageController: controller,
+            builder: (context, index) {
+              final content =
+                  archive.files.elementAt(index).rawContent as InputStream;
+              return PhotoViewGalleryPageOptions(
+                controller: _photoViewController,
+                imageProvider: MemoryImage(content.toUint8List()),
+                filterQuality: FilterQuality.high,
+                initialScale: PhotoViewComputedScale.contained,
+              );
+            }));
   }
 
   double viewportFractionBasedOnScreen(BuildContext context) {
