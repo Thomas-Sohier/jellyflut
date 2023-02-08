7c2b1a7b2032fc31fa05d1fccc0b875e187ea181
icecu8e <icecu8e44@gmail.com>
2023-02-08 22:22:28 +0100

    updated builder runner to execute also on root

--- builder_runner_all.sh
+++ builder_runner_all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script will run the build_runner for all the packages in the project
+# It will also run the pub get for all the packages
+# First we run the pub get and build_runner for the root
+flutter pub get &>/dev/null && echo "Running pub get for root"
+echo "Running build_runner for root"
+flutter pub run build_runner build --delete-conflicting-outputs > /dev/null
+
+# Then we run the pub get and build_runner for all the packages
 for dir in ./packages/*;
 do 
     (
