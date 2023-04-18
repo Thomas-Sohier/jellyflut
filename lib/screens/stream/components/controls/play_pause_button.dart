1821d04816f2a546b6e65ec689f96b755dce2578
hwr12 <ruehyeon12@gmail.com>
2023-04-19 04:42:56 +0900

    reversed play puase button diplay

--- lib/screens/stream/components/controls/play_pause_button.dart
+++ lib/screens/stream/components/controls/play_pause_button.dart
@@ -18,7 +18,7 @@ class PlayPauseButton extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Icon(
-              state.playing ? Icons.play_arrow : Icons.pause,
+              state.playing ? Icons.pause : Icons.play_arrow,
               color: Colors.white,
               size: size,
             ),
